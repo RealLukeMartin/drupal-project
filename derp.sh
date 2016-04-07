077c66a6a60f4bf48f8aed668f048c05dd91d903
RealLukeMartin <lukemartinwebdev@gmail.com>
2016-04-07 13:56:34 -0500

    local dev to prod deploy test deploy script

--- derp.sh
+++ derp.sh
@@ -2,4 +2,11 @@
 composer install
 composer update
 cp example.settings.php ./web/sites/default/settings.php
+if [ "$1" = "deploy"  ]; then
+# Proof of concept for local to prod deploys, assumes deis remote already added and environment vars already set
 cp example.settings.local.php ./web/sites/default/settings.local.php
+git add .
+git commit -m "$2"
+git push deis "$3"
+rm ./web/sites/default/settings.local.php
+fi
