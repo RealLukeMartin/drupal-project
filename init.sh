82e32da6cbf66396174df1ee38ef054d7f66daf3
RealLukeMartin <lukemartinwebdev@gmail.com>
2016-04-08 13:31:18 -0500

    rework fs and db init

--- init.sh
+++ init.sh
@@ -4,18 +4,21 @@ if [ "$1" = "deploy" ]; then
   # Only run this to get site up to cluster initially, after the intial deploy just run "git push deis master" 
   git checkout $3
   chmod -R 775 ./web/sites/default/
-  cp example.settings.php ./web/sites/default/settings.php
   cp example.settings.local.php ./web/sites/default/settings.local.php
+  printf "if (file_exists(__DIR__ . '/settings.local.php')) {" >> ./web/sites/default/settings.php
+  printf "include __DIR__ . '/settings.local.php';" >> ./web/sites/default/settings.php
+  printf "}" >> ./web/sites/default/settings.php >> ./web/sites/default/settings.php
   git add .
   git commit -m "$2"
   git push deis "$3"
-  deis open
-elif [[ "$1" = "db" ]]; then
+  echo 'importing database'
   export $(cat .env | grep -v ^# | xargs)
   vagrant ssh --command "mysqldump -u $MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD --databases $MYSQL_DATABASE_NAME" > $MYSQL_DATABASE_NAME.sql
+  kubectl exec mysql -i -- mysql -u $MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD -h $MYSQL_DATABASE_HOST --execute="CREATE DATABASE $MYSQL_DATABASE_NAME;"
   kubectl exec mysql -i -- mysql -u $MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD -h $MYSQL_DATABASE_HOST $MYSQL_DATABASE_NAME < $MYSQL_DATABASE_NAME.sql
   rm $MYSQL_DATABASE_NAME.sql
 else 
   composer install
   composer update
+
 fi
