05d7d30c9732d90c3807e073eb42527733899286
RealLukeMartin <lukemartinwebdev@gmail.com>
2016-04-08 13:52:56 -0500

    simplify init and update documentation

--- README.md
+++ README.md
@@ -12,11 +12,26 @@ If you need to create a new site using our houston platform. just run this comma
 
 Initialize remote cluster environment: 
 
-`./init.sh deploy [commit message] [branch]`
-
-Assign environment variables:
+Assign environment variables in a .env file:
 
 `MYSQL_DATABASE_NAME`
 `MYSQL_DATABASE_USER`
 `MYSQL_DATABASE_PASSWORD`
 `MYSQL_DATABASE_HOST`
+
+Create remote for site to push to: `deis create [sitename]`
+
+Set environment variables: `deis config:push`
+
+Initialize site: `./init.sh deploy [branch]`
+
+Go to your site: `deis open`
+
+---
+
+After running the init.sh script to initialize, all future deploys will use
+
+`git push deis [branch]`
+
+Sync config from local to remote with Drupal's Configuration Synchronization
+
--- init.sh
+++ init.sh
@@ -2,15 +2,15 @@
 if [ "$1" = "deploy" ]; then
   # Proof of concept for local to prod deploys, assumes deis remote already added and environment vars already set,
   # Only run this to get site up to cluster initially, after the intial deploy just run "git push deis master" 
-  git checkout $3
+  git checkout $2
   chmod -R 775 ./web/sites/default/
   cp example.settings.local.php ./web/sites/default/settings.local.php
   printf "if (file_exists(__DIR__ . '/settings.local.php')) {" >> ./web/sites/default/settings.php
   printf "include __DIR__ . '/settings.local.php';" >> ./web/sites/default/settings.php
   printf "}" >> ./web/sites/default/settings.php >> ./web/sites/default/settings.php
   git add .
-  git commit -m "$2"
-  git push deis "$3"
+  git commit -m 'init remote'
+  git push deis $2
   echo 'importing database'
   export $(cat .env | grep -v ^# | xargs)
   vagrant ssh --command "mysqldump -u $MYSQL_DATABASE_USER -p$MYSQL_DATABASE_PASSWORD --databases $MYSQL_DATABASE_NAME" > $MYSQL_DATABASE_NAME.sql
